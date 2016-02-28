fb91ab92a6702ba223a37e761c5f4cd0231c7283
Matt Wade <mdwade@gmail.com>
2016-02-28 19:57:23 +0000

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 10) 
@@ -48,5 +50,12 @@ class ArticlesController < ApplicationController
     
     def article_params
       params.require(:article).permit(:title, :description, :street, :city, :zip, :subdivision, :node, :key, :print)
+    end
+    
+    def require_same_user
+      if current_user != @article.user
+      flash[:danger] = "You do not have permission to edit this PDR!"
+      redirect_to root_path
+      end
     end
 end
